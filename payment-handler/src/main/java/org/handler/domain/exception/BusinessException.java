393f6fdfa861340ba5aa59bcc7ca33b8ee3a0ac4
wladysson <wladysonaraujo991@gmail.com>
2026-05-23 23:31:29 -0300

    feat: adiciona BusinessException para regras de negocio

---
+++ payment-handler/src/main/java/org/handler/domain/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package org.handler.domain.exception;
+
+public class BusinessException
+        extends RuntimeException {
+
+    public BusinessException(
+            String message
+    ) {
+        super(message);
+    }
+}
