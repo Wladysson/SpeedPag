bb54762be44a19b799acca14ca94f83a12258248
wladysson <wladysonaraujo991@gmail.com>
2026-05-23 23:21:51 -0300

    feat: adiciona ReceivePaymentUseCase para recebimento de pagamentos

--- payment-handler/src/main/java/org/handler/application/usecase/ReceivePaymentUseCase.java
+++ payment-handler/src/main/java/org/handler/application/usecase/ReceivePaymentUseCase.java
@@ -1,1 +1,30 @@
+package org.handler.application.usecase;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import org.handler.application.service.PaymentCommandService;
+import org.handler.application.validator.PaymentValidator;
+import org.handler.domain.model.Payment;
+import org.handler.domain.model.PaymentStatus;
+
+@ApplicationScoped
+public class ReceivePaymentUseCase {
+
+    @Inject
+    PaymentValidator paymentValidator;
+
+    @Inject
+    PaymentCommandService paymentCommandService;
+
+    public Payment execute(Payment payment) {
+
+        paymentValidator.validate(payment);
+
+        payment.setStatus(
+                PaymentStatus.PROCESSING
+        );
+
+        return paymentCommandService.create(payment);
+    }
+}
